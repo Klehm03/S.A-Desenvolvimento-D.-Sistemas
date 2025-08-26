b69cc56401210665f800f6a03e306cf86dd3ea2c
Klehm03 <isaque_klehm@estudante.sesisenai.org.br>
2025-08-26 16:58:01 -0300

    Update verificar-login.php

--- verificar-login.php
+++ verificar-login.php
@@ -58,6 +58,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo json_encode(["success" => true, "tipo" => "admin"]);
                 exit;
 
+            }else{
+                throw new Exception("Senha incorreta.");
             }
         }
         
